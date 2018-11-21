cbf45452ca193f5e6aa8d98aa7f92695f0523d1a
xuzhou524 <xuuohz@163.com>
2018-11-21 11:11:49 +0800

    WJBlueListsWatch

---
+++ WJBlueListsWatch Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  WJBlueListsWatch Extension
+//
+//  Created by gozap on 2018/11/21.
+//  Copyright © 2018 wenjuan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ WJBlueListsWatch Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WJBlueListsWatch Extension
+//
+//  Created by gozap on 2018/11/21.
+//  Copyright © 2018 wenjuan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
